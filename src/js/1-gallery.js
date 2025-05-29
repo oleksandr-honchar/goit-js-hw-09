cfb4e9dc4ecbd385990a565ee7549d416f8898da
Oleksandr Honchar <figules@gmail.com>
2025-05-29 12:28:57 +0200

    Remove unused CSS files and update gallery styles; add event listener for SimpleLightbox

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -101,3 +101,8 @@ const lightbox = new SimpleLightbox('.gallery a', {
   closeMarkup:
     '<button class="sl-close" aria-label="Close"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19.41 6.58L17.83 5l-5.42 5.42L7.99 5l-1.58 1.58L11.42 12l-5.01 5.01L7.99 19l5.42-5.42L18.83 19l1.58-1.58L13.58 12z"/></svg></button>',
 });
+
+// Optional: Add event listener for lightbox events
+lightbox.on('show.simplelightbox', () => {
+  console.log('Lightbox is shown');
+});
